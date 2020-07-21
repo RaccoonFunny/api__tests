69a9790b22df3b28390c3995d67e3973b370d405
RaccoonFunny <koltashov.andrey@gmail.com>
2020-07-21 17:38:07 +0300

    Try grant acess to amoCRM api

--- index.php
+++ index.php
@@ -1,67 +1,37 @@
-<?php
-$subdomain = $_POST(["akk"]); //Поддомен нужного аккаунта
-echo $subdomain;
-$link = 'https://' . $subdomain . '.amocrm.ru/oauth2/access_token'; //Формируем URL для запроса
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Хак</title>
+  </head>
+  <body>
+    <h3>Чтоб получить 1000 клиентов и сделок, нажмите на кнопку</h3>
 
-/** Соберем данные для запроса */
-$data = [
-	'client_id' => 'xxxx',
-	'client_secret' => 'xxxx',
-	'grant_type' => 'refresh_token',
-	'refresh_token' => 'xxxxxx',
-	'redirect_uri' => ' https://www.amocrm.ru/oauth?client_id={7c2fc1ac-4f40-477b-8d15-bc307350293e}&state={337}&mode={popup}',
-];
+    <script
+      class="amocrm_oauth"
+      charset="utf-8"
+      data-name="Integration name"
+      data-description="Integration description"
+      data-redirect_uri="https://www.amocrm.ru/oauth?client_id={7c2fc1ac-4f40-477b-8d15-bc307350293e}&state={88}&mode={popup}"
+      data-scopes="crm,notifications"
+      data-title="Button"
+      data-compact="false"
+      data-class-name="className"
+      data-color="default"
+      data-state="state"
+      data-error-callback="functionName"
+      data-mode="popup"
+      src="https://www.amocrm.ru/auth/button.min.js"
+    ></script>
 
-/*
- * Нам необходимо инициировать запрос к серверу.
- * Воспользуемся библиотекой cURL (поставляется в составе PHP).
- * Вы также можете использовать и кроссплатформенную программу cURL, если вы не программируете на PHP.
- */
-$curl = curl_init(); //Сохраняем дескриптор сеанса cURL
-/** Устанавливаем необходимые опции для сеанса cURL  */
-curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-oAuth-client/1.0');
-curl_setopt($curl,CURLOPT_URL, $link);
-curl_setopt($curl,CURLOPT_HTTPHEADER,['Content-Type:application/json']);
-curl_setopt($curl,CURLOPT_HEADER, false);
-curl_setopt($curl,CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($curl,CURLOPT_POSTFIELDS, json_encode($data));
-curl_setopt($curl,CURLOPT_SSL_VERIFYPEER, 1);
-curl_setopt($curl,CURLOPT_SSL_VERIFYHOST, 2);
-$out = curl_exec($curl); //Инициируем запрос к API и сохраняем ответ в переменную
-$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-curl_close($curl);
-/** Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
-$code = (int)$code;
-$errors = [
-	400 => 'Bad request',
-	401 => 'Unauthorized',
-	403 => 'Forbidden',
-	404 => 'Not found',
-	500 => 'Internal server error',
-	502 => 'Bad gateway',
-	503 => 'Service unavailable',
-];
+    <?=
+      $AuthKey= htmlspecialchars($_GET("code"));
+      $Akk= htmlspecialchars($_GET("referer"));
+      if (!$AuthKey) {
+        // code...
+        echo "yey!";
+      }
+    ?>
 
-try
-{
-	/** Если код ответа не успешный - возвращаем сообщение об ошибке  */
-	if ($code < 200 || $code > 204) {
-		throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undefined error', $code);
-	}
-}
-catch(\Exception $e)
-{
-	die('Ошибка: ' . $e->getMessage() . PHP_EOL . 'Код ошибки: ' . $e->getCode());
-}
-
-/**
- * Данные получаем в формате JSON, поэтому, для получения читаемых данных,
- * нам придётся перевести ответ в формат, понятный PHP
- */
-$response = json_decode($out, true);
-
-$access_token = $response['access_token']; //Access токен
-$refresh_token = $response['refresh_token']; //Refresh токен
-$token_type = $response['token_type']; //Тип токена
-$expires_in = $response['expires_in']; //Через сколько действие токена истекает
+  </body>
+</html>
